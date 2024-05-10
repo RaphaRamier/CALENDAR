1b94b5274640daaaa0bfa82e04337ea3c28bacb2
RaphaRamier <rapha.ramier@gmail.com>
2024-05-10 11:53:59 -0300

    refactor: login message error and views/login function

--- user/views.py
+++ user/views.py
@@ -14,18 +14,18 @@ def login(request):
             name = form['username'].value()
             password = form['password'].value()
 
-        usuario = auth.authenticate(
+        user = auth.authenticate(
             request,
             username = name,
             password = password
         )
 
-        if usuario is not None:
-            auth.login(request, usuario)
+        if user is not None:
+            auth.login(request, user)
             messages.success(request, 'You are successfully logged in!')
             return redirect('index')
         else:
-            messages.error(request, 'Incorrect username or password')
+            messages.error(request, 'Incorrect username or password', extra_tags='danger')
             return redirect('login')
 
     return render(request, 'user/login.html', {'form': form})
